a759c7ccc7d69d127b10b7a0931474c528d1f2b8
Shivansh Sharma <70209876+Shivansh-Sharma-GitHub@users.noreply.github.com>
2025-02-14 01:48:16 +0530

    Update deepfake_detector.py

--- deepfake_detector.py
+++ deepfake_detector.py
@@ -11,6 +11,7 @@ def preprocess_image(img):
     img = img.resize((224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0) 
+    img_array = img_array / 255.0
     
 
 st.title("Deepfake Detection")
